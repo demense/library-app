aff46b2aab483f9a187d6a6fd81f6438a4f0f758
Ahmed <ahmedadil9102@gmail.com>
2025-04-08 17:04:04 +0300

    Add Book constructor and function to create and store books with unique IDs

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("hi dad")
+// Book objects are stored in this array
+const library = [];
+
+// Constructor for book objects
+function Book(title, author, pages, read, id) {
+  if (!new.target) {
+    throw Error("You should call the constructor with 'new'");
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = id;
+}
+
+// This function adds books to the "library" array
+function addBookToLibrary(title, author, pages, read) {
+  const newBookId = crypto.randomUUID();
+  const newBook = new Book(title, author, pages, read, newBookId);
+  library.push(newBook);
+}
+
+const testBook = addBookToLibrary("test","test","23","Yes");
+
+console.log(library)
