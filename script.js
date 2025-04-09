d3acb6419ac292fde0f037f396afbf005debaf4c
Ahmed <ahmedadil9102@gmail.com>
2025-04-09 11:16:49 +0300

    Add “New Book” button with form to input and add books to library

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ function Book(title, author, pages, read, id) {
   this.id = id;
 }
 
+showForm();
+
+// ######### Function Declarations #########
+
 // This function add books to the library
 function addBookToLibrary(title, author, pages, read) {
   // Generate random ID for the book
@@ -25,7 +29,9 @@ function addBookToLibrary(title, author, pages, read) {
 
 // This function renders books
 function renderBooks() {
+  console.log(library);
   const tbody = document.querySelector("tbody");
+  tbody.textContent = "";
 
   for (let i = 0; i < library.length; i++) {
     const tr = document.createElement("tr");
@@ -46,13 +52,103 @@ function renderBooks() {
     const read = document.createElement("td");
     read.textContent = library[i].read;
     tr.appendChild(read);
-
-    console.log(library[i]);
   }
 }
 
-const testBook1 = addBookToLibrary("test", "test", "23", "Yes");
-const testBook2 = addBookToLibrary("test", "test", "23", "Yes");
-const testBook3 = addBookToLibrary("test", "test", "23", "Yes");
+// This function open a dialog with a form to fill new book info
+function showForm() {
+  const newBookBtn = document.querySelector("#new-book-btn");
+  const dialog = document.querySelector("dialog");
+  newBookBtn.addEventListener("click", function () {
+    dialog.showModal();
+    dialog.textContent = "";
 
-renderBooks();
+    // Create form
+    const form = document.createElement("form");
+    dialog.appendChild(form);
+
+    // title label
+    const titleLabel = document.createElement("label");
+    titleLabel.setAttribute("for", "title");
+    titleLabel.textContent = "Title";
+    form.appendChild(titleLabel);
+
+    // title input
+    const titleInput = document.createElement("input");
+    titleInput.setAttribute("type", "text");
+    titleInput.setAttribute("id", "title");
+    titleInput.required = true;
+    titleLabel.appendChild(titleInput);
+
+    // author label
+    const authorLabel = document.createElement("label");
+    authorLabel.setAttribute("for", "author");
+    authorLabel.textContent = "Author";
+    form.appendChild(authorLabel);
+
+    // author input
+    const authorInput = document.createElement("input");
+    authorInput.setAttribute("type", "text");
+    authorInput.setAttribute("id", "author");
+    authorInput.required = true;
+    authorLabel.appendChild(authorInput);
+
+    // pages label
+    const pagesLabel = document.createElement("label");
+    pagesLabel.setAttribute("for", "pages");
+    pagesLabel.textContent = "Pages";
+    form.appendChild(pagesLabel);
+
+    // pages input
+    const pagesInput = document.createElement("input");
+    pagesInput.setAttribute("type", "number");
+    pagesInput.setAttribute("id", "pages");
+    pagesInput.required = true;
+    pagesLabel.appendChild(pagesInput);
+
+    // read label
+    const readLabel = document.createElement("label");
+    readLabel.setAttribute("for", "read");
+    readLabel.textContent = "Read ";
+    form.appendChild(readLabel);
+
+    // read input
+    const select = document.createElement("select");
+    select.setAttribute("id", "read");
+    readLabel.appendChild(select);
+    const yes = document.createElement("option");
+    yes.value = "Yes";
+    yes.textContent = "Yes";
+    select.appendChild(yes);
+    const no = document.createElement("option");
+    no.value = "No";
+    no.textContent = "No";
+    select.appendChild(no);
+
+    const submitBtn = document.createElement("button");
+    submitBtn.setAttribute("type", "submit");
+    submitBtn.setAttribute("id", "book-submit-btn");
+    submitBtn.textContent = "Submit";
+    form.appendChild(submitBtn);
+
+    // Add filled book to library
+    submitBtn.addEventListener("click", function (event) {
+      event.preventDefault();
+      dialog.close();
+      addBookToLibrary(
+        titleInput.value,
+        authorInput.value,
+        pagesInput.value,
+        select.value
+      );
+      renderBooks();
+    });
+  });
+}
+
+// #########################################
+
+// Testing
+// const testBook1 = addBookToLibrary("test", "test", "23", "Yes");
+// const testBook2 = addBookToLibrary("test", "test", "23", "Yes");
+// const testBook3 = addBookToLibrary("test", "test", "23", "Yes");
